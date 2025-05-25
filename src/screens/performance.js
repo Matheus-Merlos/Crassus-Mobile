9a95a8e88cdc064a98af9ff522a61c8beeda85bd
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-05-25 16:38:24 -0300

    Inicio da tela de desempenho, com gráficos e itens iniciais

--- src/screens/performance.js
+++ src/screens/performance.js
@@ -1,5 +1,85 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import WhiteIshBackground from "../components/whiteIshBackground";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 
 export default function PerformanceScreen() {
-  return <Text>Iei!</Text>;
+  const mockData = {
+    totalCalories: 2238,
+    consumedCalories: 1541,
+    missingCalories: 697,
+    burntCalories: 561,
+  };
+  return (
+    <WhiteIshBackground
+      title="Hoje"
+      titleDistanceToTop={75}
+      screenPercentage={83}
+      paddingTop={30}
+    >
+      <View style={styles.resume}>
+        <View style={styles.resumeItem}>
+          <Text style={styles.resumeItemTitle}>
+            {mockData.consumedCalories}
+          </Text>
+          <Text style={styles.resumeItemDescription}>Consumidas</Text>
+        </View>
+        <AnimatedCircularProgress
+          size={150}
+          width={5}
+          fill={
+            (mockData.totalCalories /
+              (mockData.totalCalories + mockData.missingCalories)) *
+            100
+          }
+          tintColor="#F50057"
+          backgroundColor="#ddd"
+        >
+          {() => {
+            return (
+              <>
+                <Text style={styles.kcalGraphNumber}>
+                  {mockData.missingCalories}
+                </Text>
+                <Text style={styles.kcalDescribe}>KCal Restante</Text>
+              </>
+            );
+          }}
+        </AnimatedCircularProgress>
+        <View style={styles.resumeItem}>
+          <Text style={styles.resumeItemTitle}>{mockData.burntCalories}</Text>
+          <Text style={styles.resumeItemDescription}>Gastas</Text>
+        </View>
+      </View>
+    </WhiteIshBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+  resume: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    gap: 30,
+  },
+  resumeItem: {
+    marginBottom: 10,
+    width: 90,
+  },
+  resumeItemTitle: {
+    fontFamily: "Poppins-SemiBold",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  resumeItemDescription: {
+    fontFamily: "Poppins-Regular",
+    textAlign: "center",
+  },
+  kcalGraphNumber: {
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 24,
+  },
+  kcalDescribe: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 16,
+    opacity: 0.5,
+  },
+});
