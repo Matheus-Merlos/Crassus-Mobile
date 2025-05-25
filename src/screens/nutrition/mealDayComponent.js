4ec2cc3a22d422dd2b2718927ca543baf5646b76
Murilo-Hasse <murilohassemh@gmail.com>
2025-05-25 01:14:08 -0300

    feat/meal-screen---no/add-meal-screen

---
+++ src/screens/nutrition/mealDayComponent.js
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import MealItem from "./mealItemItem";
+import * as colors from "../../constants/colors";
+
+export default function MealDay({ date, meals }) {
+  return (
+    <View style={styles.mealDay}>
+      <Text style={styles.mealDate}>{date}</Text>
+      {meals.map((meal, index) => (
+        <MealItem
+          key={index}
+          name={meal.name}
+          calories={meal.calories}
+          iconIndex={1}
+        />
+      ))}
+    </View>
+  );
+}
+
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  mealDay: {
+    width: width,
+    backgroundColor: colors.WHITE_ISH,
+    padding: 15,
+    marginBottom: 20,
+  },
+  mealDate: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: colors.BACKGROUND_YELLOW,
+    marginBottom: 10,
+  },
+});
---
+++ src/screens/nutrition/nutrition.js
@@ -0,0 +1,105 @@
+import { useNavigation } from "@react-navigation/native";
+import { Text, ScrollView, StyleSheet, Dimensions, View } from "react-native";
+import * as colors from "../../constants/colors";
+import MealDay from "./mealDayComponent";
+import CrassusButton from "../../components/crassusButton";
+
+export default function MealsScreen() {
+  const navigation = useNavigation();
+  const mockMeals = [
+    {
+      date: "17/04/2025",
+      meals: [
+        { name: "Jantar 1", calories: 379 },
+        { name: "Almoço 1", calories: 856 },
+        { name: "Café da Manhã 1", calories: 76 },
+      ],
+    },
+    {
+      date: "16/04/2025",
+      meals: [
+        { name: "Jantar 1", calories: 721 },
+        { name: "Almoço 1", calories: 1152 },
+        { name: "Café da Manhã 1", calories: 76 },
+      ],
+    },
+    {
+      date: "16/04/2025",
+      meals: [
+        { name: "Jantar 1", calories: 721 },
+        { name: "Almoço 1", calories: 1152 },
+        { name: "Café da Manhã 1", calories: 76 },
+      ],
+    },
+    {
+      date: "16/04/2025",
+      meals: [
+        { name: "Jantar 1", calories: 721 },
+        { name: "Almoço 1", calories: 1152 },
+        { name: "Café da Manhã 1", calories: 76 },
+      ],
+    },
+  ];
+
+  const handleNewMeal = () => {
+    navigation.navigate("AddMeal");
+  };
+
+  return (
+    <>
+      <Text style={styles.title}>Suas Refeições</Text>
+      <ScrollView contentContainerStyle={styles.background}>
+        {mockMeals.map((day, index) => (
+          <MealDay key={index} date={day.date} meals={day.meals} />
+        ))}
+      </ScrollView>
+      <View style={styles.button}>
+        <CrassusButton
+          text="Nova refeição"
+          color={colors.BACKGROUND_YELLOW}
+          style={styles.addButton}
+          onPress={handleNewMeal}
+        />
+      </View>
+    </>
+  );
+}
+
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  title: {
+    position: "absolute",
+    fontFamily: "PublicSans-Bold",
+    color: colors.WHITE,
+    fontSize: 40,
+    left: 30,
+    top: 80,
+  },
+  background: {
+    marginTop: 150,
+    position: "absolute",
+    width,
+    height: height * 0.8,
+    top: 20,
+    backgroundColor: colors.WHITE_ISH,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    paddingTop: 25,
+    alignItems: "center",
+  },
+  addButton: {
+    position: "absolute",
+    bottom: 100,
+    alignSelf: "center",
+    backgroundColor: "#fbbc05",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    width: width * 0.6,
+  },
+  button: {
+    height: 150,
+    justifyContent: "center",
+    backgroundColor: colors.WHITE_ISH,
+  },
+});
