d85bfe14548e84a6f4083796ae6a95a85c1d72c4
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-05-24 23:54:01 -0300

    Configuração do React Navigation para as telas de login

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import { registerRootComponent } from "expo";
 
 import App from "./App";
+import { Provider } from "react-redux";
+import { loginStore, loginPersistor } from "./src/redux/stores";
+import { PersistGate } from "redux-persist/integration/react";
+
+function Root() {
+  return (
+    <Provider store={loginStore}>
+      <PersistGate loading={null} persistor={loginPersistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Root);
