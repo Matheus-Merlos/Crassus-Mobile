18196b1d3feb8a14a5a235c1988da23b96e63241
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-01 21:50:29 -0300

    Configurações iniciais de notificações Toast

--- App.js
+++ App.js
@@ -12,6 +12,53 @@ import { isLoadingAtom } from "./src/jotai/store";
 
 import AppNavigator from "./src/navigation/AppNavigator";
 import Spinner from "react-native-loading-spinner-overlay";
+import Toast, { BaseToast, ErrorToast } from "react-native-toast-message";
+import Icon from "react-native-vector-icons/Feather";
+
+const toastConfig = {
+  success: (props) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: "green" }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{ fontSize: 20, fontWeight: "bold" }}
+      text2Style={{ fontSize: 14 }}
+      renderLeadingIcon={() => (
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: 10,
+            marginRight: 10,
+          }}
+        >
+          <Icon name="check-circle" size={24} color="green" />
+        </View>
+      )}
+    />
+  ),
+  error: (props) => (
+    <ErrorToast
+      {...props}
+      style={{ borderLeftColor: "red" }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{ fontSize: 14, fontFamily: "Inter-Bold" }}
+      text2Style={{ fontSize: 14 }}
+      renderLeadingIcon={() => (
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: 10,
+            marginRight: 5,
+          }}
+        >
+          <Icon name="x-circle" size={24} color="red" />
+        </View>
+      )}
+    />
+  ),
+};
 
 export default function App() {
   const [isLoading] = useAtom(isLoadingAtom);
@@ -37,6 +84,7 @@ export default function App() {
         animation="fade"
         overlayColor="rgba(0,0,0,0.6)"
       />
+      <Toast config={toastConfig} />
     </View>
   );
 }
